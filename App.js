a842be6ba078dc06f87d4ffffa773c7fb7b0ca82
Chissanu <chissanuoak2@gmail.com>
2023-01-06 14:48:44 +0700

    Add dependencies

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+var dummyData = {
+  "name" : "hello",
+  "age" : 15
+}
+
 export default function App() {
   return (
     <View style={styles.container}>
